ac0259b6a3189ceea6bfc8294fabc3489c33cf27
marineags <agsmarine23@gmail.com>
2025-11-17 12:15:10 +0100

    creat function addQuoteAuthor

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+const form = document.getElementById("form");
 const btn = document.getElementById("button");
 
-btn.addEventListener("click", () => {
+function addQuoteAuthor(text, author) {
+  const quoteText = document.createElement("p");
+  quoteText.classList.add("text");
+  quoteText.textContent = text;
+  //cree un p pour la citation
+
+  const quoteAuthor = document.createElement("p");
+  quoteAuthor.classList.add("author");
+  quoteAuthor.textContent = author;
+  //cree un p pour l'auteur
+
+  const quoteContainer = document.createElement("div");
+  quoteContainer.classList.add("quote");
+  //cree une nouvelle div
+
+  quoteContainer.appendChild(quoteText);
+  quoteContainer.appendChild(quoteAuthor);
+  //je range mes p dans la div
+
+  const list = document.getElementById("quote-list");
+  //pour les mettre a l'endroit demandé
+  list.appendChild(quoteContainer);
+  //et je la range
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // empêche la page de se recharger
+
   const text = document.getElementById("addQuote").value;
   const author = document.getElementById("author").value;
 
-  console.log(text, author);
+  // si un des champs est vide, on ne fait rien
+  if (!text || !author) return;
+
+  addQuoteAuthor(text, author);
+
+  // vider les champs du formulaire
+  form.reset();
 });
